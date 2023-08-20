94e94a8f8325becb9f5dff4f5b3aff73ab483868
Aleksandr Sokolov <sokolovmono@gmail.com>
2023-08-20 19:30:28 +0300

    add some constants

---
+++ src/util/constants.js
@@ -0,0 +1,14 @@
+export const BUDGET = 15;
+export const MOVIE_COST = 3;
+
+export const options = {
+    method: "GET",
+    headers: {
+        accept: "application/json",
+        Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZWVlYjIyODFmYjg5Y2Y1Y2JiNDk2NGVmZWZmZTc0YiIsInN1YiI6IjY0ZGNkYzQ0MzcxMDk3MDExYzUyNDZhYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hKGQKcnYHhnVE4Xz05yMmQaYvI64QnH0Cv-zSKfBfro"
+    }
+};
+
+export const imageLink = "https://image.tmdb.org/t/p/w500"
+
+export const MoviesListAPILink = "https://api.themoviedb.org/3/trending/movie/day?language=en-US'"
