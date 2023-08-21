a32717504a9f8fbcd42c5604e2e5289ab8e49212
Aleksandr Sokolov <sokolovmono@gmail.com>
2023-08-21 13:47:28 +0300

    add fetchMovieDetailsData.js

---
+++ src/utils/fetchMovieDetailsData.js
@@ -0,0 +1,18 @@
+import {IMG_API_LINK, LANGUAGE_PART_OF_API_LINK, MOVIE_DETAIL_API_LINK, options} from "./constants";
+
+const fetchMovieDetailsData = (movieID) => {
+    return fetch(`${MOVIE_DETAIL_API_LINK}${movieID}${LANGUAGE_PART_OF_API_LINK}`, options)
+        .then(response => response.json())
+        .then(data => {
+            const year = new Date(data.release_date).getFullYear();
+            return {
+                id: data.id,
+                title: data.title,
+                img: IMG_API_LINK + data.poster_path,
+                description: data.overview,
+                year: year
+            };
+        });
+};
+
+export default fetchMovieDetailsData;
