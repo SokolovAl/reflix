67dc02b9bac6a8b29ccbaf5e4b19e7f4f6c938c0
Aleksandr Sokolov <sokolovmono@gmail.com>
2023-08-21 16:42:20 +0300

    some refactor

--- src/utils/fetchMovieTrailer.js
+++ src/utils/fetchMovieTrailer.js
@@ -1,4 +1,4 @@
-import {IMG_API_LINK, LANGUAGE_PART_OF_API_LINK, MOVIE_DETAIL_API_LINK, options} from "./constants";
+import {LANGUAGE_PART_OF_API_LINK, MOVIE_DETAIL_API_LINK, options} from "./constants";
 
 const fetchMovieTrailer = (movieID) => {
     return fetch(`${MOVIE_DETAIL_API_LINK}${movieID}/videos${LANGUAGE_PART_OF_API_LINK}`, options)
