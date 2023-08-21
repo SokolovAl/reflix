6eb140a5a83da33964c4a22b3736fb8d3e90d3d0
Aleksandr Sokolov <sokolovmono@gmail.com>
2023-08-21 13:48:18 +0300

    refactor app file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import "./style/App.css";
-import {Router} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./routes/Home";
+import Catalog from "./routes/Catalog";
+import MovieDetails from "./routes/MovieDetails";
 
 function App() {
     return (
         <Router>
+            <div className = "navbar">
+                <NavBar/>
+            </div>
+            <Routes>
+                <Route path = "/" element = {<Home/>}></Route>
+                <Route path = "/catalog/:userId" element = {<Catalog/>}></Route>
+                <Route path = "/catalog/:userId/:movieId" element = {<MovieDetails/>}></Route>
+            </Routes>
         </Router>
     );
 }
